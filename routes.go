fe543108947a846585ba2c19c365ae9908156b66
sudarshan.ArchMachine <sudar.theone@gmail.com>
2016-08-27 20:16:41 +0530

    refactoring

--- routes.go
+++ routes.go
@@ -6,19 +6,26 @@ import (
 	"net/url"
 )
 
+func gateWay(w http.ResponseWriter, r *http.Request) (url.Values, bool) {
+	values, err := url.ParseQuery(r.URL.RawQuery)
+	if errReturn(err, "", w) {
+		return nil, true
+	}
+	if len(values.Get("key")) == 0 {
+		if errReturn(nil, "Wrong input key", w) {
+			return nil, true
+		}
+	}
+	return values, false
+}
+
 func get(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		values, err := url.ParseQuery(r.URL.RawQuery)
-		if errReturn(err, "", w) {
+		values, hasErrors := gateWay(w, r)
+		if hasErrors {
 			return
 		}
-		if len(values.Get("key")) == 0 {
-			if errReturn(nil, "Wrong input key", w) {
-				return
-			}
-		}
-
-		fmt.Println(values.Get("key"))
+		fmt.Println("in")
 		bookMutex.RLock()
 		value := bookstore[string(values.Get("key"))]
 		bookMutex.RUnlock()
@@ -34,6 +41,9 @@ func get(w http.ResponseWriter, r *http.Request) {
 }
 
 func set(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+
+	}
 }
 
 func remove(w http.ResponseWriter, r *http.Request) {
