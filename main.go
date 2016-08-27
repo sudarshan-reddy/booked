6cedbbe6badeb0ac5d296c56cb0eb77cd3f16449
sudarshan.ArchMachine <sudar.theone@gmail.com>
2016-08-27 11:55:29 +0530

    first commit

--- main.go
+++ main.go
@@ -17,15 +17,3 @@ func main() {
 	http.HandleFunc("/list", list)
 	http.ListenAndServe(":3000", nil)
 }
-
-func get(w http.ResponseWriter, r *http.Request) {
-}
-
-func set(w http.ResponseWriter, r *http.Request) {
-}
-
-func remove(w http.ResponseWriter, r *http.Request) {
-}
-
-func list(w http.ResponseWriter, r *http.Request) {
-}
---
+++ routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func get(w http.ResponseWriter, r *http.Request) {
+}
+
+func set(w http.ResponseWriter, r *http.Request) {
+}
+
+func remove(w http.ResponseWriter, r *http.Request) {
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+}
